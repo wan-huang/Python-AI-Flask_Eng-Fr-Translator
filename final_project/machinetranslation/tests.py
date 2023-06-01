12e5ae52a3753246c9afefe1f5e03b8dfc71e8f0
wan-huang <wan.huang@mail.mcgill.ca>
2023-06-01 09:27:48 -0400

    pylint score 10

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,14 +1,14 @@
 import unittest
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
 class TestEnglish2French(unittest.TestCase):
-    def test_englishToFrench(self):
-        self.assertEqual(englishToFrench('english'),'anglaise')
-        self.assertEqual(englishToFrench('cat'),'chat')
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french('sun'),'soleil')
+        self.assertEqual(english_to_french('cat'),'chat')
 
 class TestFrench2English(unittest.TestCase):
-    def frenchToEnglish(self):
-        self.assertEqual(frenchToEnglish('français'),'french')
-        self.assertEqual(frenchToEnglish('chien'),'dog')
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english('lune'),'moon')
+        self.assertEqual(french_to_english('chien'),'dog')
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,18 @@
+"""
+this module implements eng-2-fr and fr-2-end translator functions
+"""
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(englishText):
-    #write the code here
-    frenchText = MyMemoryTranslator(source='en', target='fr').translate(englishText)
-    return frenchText
+def english_to_french(english_text):
+    """
+    englisth to french translation function 
+    """
+    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
+    return french_text
 
-def frenchToEnglish(frenchText):
-    #write the code here
-    englishText = MyMemoryTranslator(source='en', target='fr').translate(frenchText)
-    return englishText
+def french_to_english(french_text):
+    """
+    french to english translation function 
+    """
+    english_text = MyMemoryTranslator(source='fr', target='en').translate(french_text)
+    return english_text

8ad7e4c6aa014532e7562317ca5cf0e18a1144f5
wan-huang <wan.huang@mail.mcgill.ca>
2023-06-01 10:17:22 -0400

    deploy app on code eng

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,11 +4,11 @@ from translator import english_to_french, french_to_english
 class TestEnglish2French(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(english_to_french('sun'),'soleil')
-        self.assertEqual(english_to_french('cat'),'chat')
+        self.assertNotEqual(english_to_french('cat'),'chien')
 
 class TestFrench2English(unittest.TestCase):
     def test_french_to_english(self):
         self.assertEqual(french_to_english('lune'),'moon')
-        self.assertEqual(french_to_english('chien'),'dog')
+        self.assertNotEqual(french_to_english('chat'),'dog')
 
 unittest.main()
